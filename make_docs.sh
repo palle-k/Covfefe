8a24fd7c1151b8a4016dd161f6085b35102f352d
Palle Klewitz <palle.klewitz@tum.de>
2018-02-21 05:51:44 +0100

    Add BNF doc to docs

--- make_docs.sh
+++ make_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jazzy --clean --author "palle-k" --author_url https://github.com/palle-k --github_url https://github.com/palle-k/Covfefe --output docs/ --theme fullwidth 
+jazzy --clean --author "palle-k" --author_url https://github.com/palle-k --github_url https://github.com/palle-k/Covfefe --output docs/ --theme fullwidth --documentation BNF.md
 rm -rf build
